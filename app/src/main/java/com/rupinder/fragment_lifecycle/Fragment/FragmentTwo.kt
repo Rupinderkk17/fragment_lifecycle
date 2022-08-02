8db24505ffb5880ee26d714fd0474af43eae8b1f
Rupinderkk17 <109845928+Rupinderkk17@users.noreply.github.com>
2022-08-02 18:41:24 +0530

    Initial commit

---
+++ app/src/main/java/com/rupinder/fragment_lifecycle/Fragment/FragmentTwo.kt
@@ -0,0 +1,42 @@
+package com.rupinder.fragment_lifecycle.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.rupinder.fragment_lifecycle.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FragmentTwo.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FragmentTwo : Fragment() {
+   private lateinit var btnNavigateTwo:Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view:View= inflater.inflate(R.layout.fragment_two, container, false)
+
+        btnNavigateTwo = view.findViewById(R.id.btnNavigateTwo)
+        btnNavigateTwo.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_fragmentTwo_to_fragmentOne)
+        }
+
+
+        return view
+    }
+
+
+}
